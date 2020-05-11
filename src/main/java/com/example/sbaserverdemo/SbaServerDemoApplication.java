4428599e4b56a79d830efee3c2b274b027f2cdd9
chengang <837718548@qq.com>
2020-05-11 15:38:10 +0800

    添加Kubernetes依赖

--- src/main/java/com/example/sbaserverdemo/SbaServerDemoApplication.java
+++ src/main/java/com/example/sbaserverdemo/SbaServerDemoApplication.java
@@ -3,9 +3,13 @@ package com.example.sbaserverdemo;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableDiscoveryClient
+@EnableScheduling
 public class SbaServerDemoApplication {
 
     public static void main(String[] args) {
